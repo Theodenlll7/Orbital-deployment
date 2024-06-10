1604c22e024f458a2b255b86c7ea04040dcdbd2b
Theodor Östling <theos327@student.liu.se>
2024-06-10 12:10:54 +0200

    Added movement with wasd keys

--- Apex_Survival_2D/Character/MainCharacter2D.gd
+++ Apex_Survival_2D/Character/MainCharacter2D.gd
@@ -18,8 +18,8 @@ func _physics_process(delta):
 	player_movement(delta)
 
 func get_input():
-	input.x = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
+	input.x = int(Input.is_action_pressed("ui_right") or Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("ui_left") or Input.is_action_pressed("move_left"))
+	input.y = int(Input.is_action_pressed("ui_down") or Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("ui_up") or Input.is_action_pressed("move_up"))
 	return input.normalized()
 
 func player_movement(delta):
