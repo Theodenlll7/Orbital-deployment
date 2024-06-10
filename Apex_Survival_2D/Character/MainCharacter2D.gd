9280c15edffa3edea7dbebf0a8dfad7dc2d3912e
Theodenlll7 <75118246+Theodenlll7@users.noreply.github.com>
2024-06-10 11:50:19 +0200

    Add files via upload

---
+++ Apex_Survival_2D/Character/MainCharacter2D.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var player_stats: PlayerStats
+
+var input = Vector2.ZERO
+
+var friction 
+var max_speed 
+var acceleration 
+
+func _ready():
+	player_stats = PlayerStats.new()
+	friction = player_stats.friction
+	max_speed = player_stats.max_speed
+	acceleration = player_stats.acceleration
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+
+func player_movement(delta):
+	input = get_input()
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction* delta):
+			velocity -= velocity.normalized()*(friction*delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity+= (input*acceleration*delta)
+		velocity = velocity.limit_length(max_speed)
+	
+	move_and_slide()
+		
---
+++ Apex_Survival_2D/Character/PlayerStats/PlayerStats.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+class_name PlayerStats
+
+@export var max_speed: float = 400
+@export var acceleration: float = 1000
+@export var friction: float = 600
