7cb442008faf20efab7fbb60c2436aea399ca420
Emil Wallberg <ew2001@live.se>
2024-07-29 17:38:18 +0200

    Projectile collision

--- weapons/projectiles/projectile_2d.gd
+++ weapons/projectiles/projectile_2d.gd
@@ -1,16 +1,26 @@
-extends Node2D
+extends RigidBody2D
 class_name Projectile_2D
 
-@export var speed: float = 500.0
+@export var impulse: float = 500.0
 @export var lifetime: float = 2.0
 
 var direction: Vector2
 
 
 func _ready() -> void:
+	set_contact_monitor(true)
+	set_max_contacts_reported(1)
+	connect("body_entered", _on_body_entered)
+	apply_impulse(direction * impulse)
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
 
 
-func _process(delta: float) -> void:
-	position += direction * speed * delta
+func _on_body_entered(body):
+	# This get dose not work need to read the documentation for clarity
+	var hp = body.get_node("HealthComponent") as HealthComponent
+	print(hp)
+	if hp:
+		hp.damage(10)
+		print(hp.current_health)
+	queue_free()
