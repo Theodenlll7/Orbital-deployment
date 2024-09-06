acd2658460489a9db037d81156b7351329d5c307
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-06 13:44:30 +0200

    Animations for character

--- player/scripts/player_movment_2d.gd
+++ player/scripts/player_movment_2d.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-@export var move_speed = 200
-@export var dodge_speed = 600
-@export var dodge_duration = 0.2
+@export var move_speed = 100
+@export var dodge_speed = 200
+@export var dodge_duration = 0.4
 @export var dodge_cooldown = 0.5
+
+@onready var animated_sprite := $AnimatedSprite2D
 
 var dodge_timer = -dodge_cooldown
 var can_dodge = true
@@ -29,11 +31,24 @@ func _process(delta):
 func move_player() -> void:
 	if dodge_timer > 0:
 		velocity = velocity.normalized() * dodge_speed
+		animated_sprite.play("roll_h")
 	else:
 		var input_vector = (
 			Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 		)
 		velocity = input_vector * move_speed
+		
+		if input_vector.x < 0:
+			animated_sprite.flip_h = true;
+		elif input_vector.x > 0:
+			animated_sprite.flip_h = false;
+
+		if !velocity.is_zero_approx():
+			animated_sprite.play("run_h")
+		else:
+			animated_sprite.play("idle")
+
+	
 
 	move_and_slide()
 
