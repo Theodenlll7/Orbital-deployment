3a3b0a59e4475d7dd46885680506809c8597a7d4
Theodor Östlin <theos327@student.liu.se>
2024-09-09 13:34:21 +0200

    fixade lite små grejer

--- maps/ship/MainHouseUI.gd
+++ maps/ship/MainHouseUI.gd
@@ -12,12 +12,17 @@ func _on_upgrade_turret_button_pressed():
 	SoundEngine.playUpgradeSound()
 
 func updateButtonLabels():
-	var turret_label_cost = $upgradeTurretButton.get_node("Cost")
-	turret_label_cost.text = str(GameVariables.getCostOfUpgrade("towerUpgrade")) + "$"
-	
 	var player_money = $Header.get_node("Players_money")
 	player_money.text = str(GameVariables.getPlayerMoney()) + "$"
 	
+	var weapon_path_upgrades = $Weapon_paths
+	for i in range(weapon_path_upgrades.get_child_count()):
+		var child = weapon_path_upgrades.get_child(i)
+		for j in range(child.get_child_count()):
+			child.get_child(j).get_child(0).text = "23"
+			child.get_child(j).get_child(0).text = "23"
+
+
 	
 func _on_pistols_button_pressed(extra_arg_0: int) -> void:
 	var weaponsUI = $weapons_UI
