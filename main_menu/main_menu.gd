67a27b76c200fdb24c474b32371dbb7e4d71b28c
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-11 13:16:35 +0200

    Continued work on menue

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,22 +1,27 @@
 class_name MainMenu
 extends Control
 
-@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
-@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
-@onready var start_level = preload("res://scenes/world.tscn") as PackedScene
+@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton 
+@onready var options_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/OptionsButton 
+@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton 
+@onready var start_level: PackedScene = preload("res://scenes/world.tscn") 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start_button.button_down.connect(on_start_button_pressed)
-	exit_button.button_down.connect(on_exit_button_pressed)
+	handle_connecting_signals()
 
 func on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
+func on_options_button_pressed() -> void:
+	print("LOAD options")
+
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_button_pressed)
+	options_button.button_down.connect(on_options_button_pressed)
+	exit_button.button_down.connect(on_exit_button_pressed)
+	
