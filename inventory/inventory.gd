18f66843072a4daa6699a8ff824ccb6b8170585b
Theodor Östlin <theos327@student.liu.se>
2024-09-12 14:44:08 +0200

    selected works now

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -21,10 +21,9 @@ func add_weapon(weapon):
 		weapon_slots[empty_slot] = weapon
 		setPlayerInventoryImage(empty_slot+1, weapon.weapon_texture_icon)
 	else:
-		selected_weapon_slot = randf_range(1, 2)
 		drop_item(weapon_slots[selected_weapon_slot])
 		weapon_slots[selected_weapon_slot] = weapon
-		setPlayerInventoryImage(selected_weapon_slot, weapon.weapon_texture_icon)
+		setPlayerInventoryImage(selected_weapon_slot+1, weapon.weapon_texture_icon)
 
 func add_explosive(explosive):
 	explosive_slot = explosive
@@ -37,14 +36,14 @@ func add_special_equipment(special_equipment):
 func drop_item(item):
 	pass
 
-#Change a sprite image
+#Change a sprite image when you change weapon
 func setPlayerInventoryImage(slot, image):
 	if image:
 		self.get_child(slot).texture = image
 	else:
 		print("Icon texture not found")
 
-#Coming from shop with the help of signal
+#Coming from shop with the help of a signal
 func _on_purchase_made(item):
 	if item is Weapon:
 		print("Purchased Weapon: ", item.weapon_name, " | Cost: ", item.weapon_cost)
@@ -56,3 +55,23 @@ func _on_purchase_made(item):
 		print("Purchased Special Equipment: ", item.equipment_name, " | Cost: ", item.equipment_cost, " | Effect: ", item.equipment_effect, " | Type: ", item.equipment_type)
 	else:
 		print("Unknown item type: ", item)
+		
+func _input(event):
+	if event.is_action_pressed("TAB"):
+		select_next_slot()
+
+func select_next_slot() -> void:
+	if weapon_slots.size() > 0:
+		selected_weapon_slot = (selected_weapon_slot + 1) % weapon_slots.size()
+		update_selected_slot()
+
+func update_selected_slot() -> void:
+	print("Selected slot:", selected_weapon_slot)
+	# Reset all slots
+	self.get_child(1).modulate = Color(1, 1, 1)  # Reset color to white
+	self.get_child(1).scale = Vector2(0.5, 0.5)  # Reset scale to normal
+	self.get_child(2).modulate = Color(1, 1, 1)  # Reset color to white
+	self.get_child(2).scale = Vector2(0.5, 0.5)  # Reset scale to normal
+	
+	self.get_child(selected_weapon_slot+1).modulate = Color(1, 1, 0)  # Highlight with yellow color
+	self.get_child(selected_weapon_slot+1).scale = Vector2(0.7, 0.7)  # Scale up the sprite
