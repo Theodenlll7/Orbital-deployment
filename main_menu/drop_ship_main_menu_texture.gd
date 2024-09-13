d1abac6bf9aaf2d3c8825c09ee414e63e4b824d2
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-13 09:24:43 +0200

    Updated tile textures

--- main_menu/drop_ship_main_menu_texture.gd
+++ main_menu/drop_ship_main_menu_texture.gd
@@ -1,7 +1,6 @@
 extends TextureRect
 
 # Variables to control the motion
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 var amplitude  = 50.0  
 var speed = 2.0     
 var start_position = Vector2()  
@@ -10,11 +9,11 @@ var time_elapsed = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	start_position = position 
-	animation_player.play("fly_in_from_left")
+	#animation_player.play("fly_in_from_left")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	if animation_player.current_animation == "":
-		time_elapsed += delta
-		position.y = start_position.y + amplitude * sin(time_elapsed * speed) 
+	#if animation_player.current_animation == "":
+	time_elapsed += delta
+	position.y = start_position.y + amplitude * sin(time_elapsed * speed) 
