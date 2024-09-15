eb4f43cfa2e80007e6d3c5f9c3c7cfc1326db399
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-15 18:39:57 +0200

    Added a theme for the menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,11 +1,15 @@
 class_name MainMenu
 extends Control
 
-@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton 
-@onready var options_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/OptionsButton 
-@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton 
-@onready var start_level: PackedScene = preload("res://scenes/world.tscn") 
+@onready var start_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/StartButton 
+@onready var options_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/OptionsButton 
+@onready var exit_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/ExitButton 
 
+@onready var options_menu: OptionsMenu = $options_menu
+@onready var content_margin_container: MarginContainer = $ContentMarginContainer
+
+
+@onready var start_level: PackedScene = preload("res://scenes/world.tscn") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,13 +19,20 @@ func on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
 func on_options_button_pressed() -> void:
-	print("LOAD options")
+	content_margin_container.visible = false
+	options_menu.visible = true
+	options_menu.set_process(true)
 
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func on_exit_options_menu() -> void:
+	content_margin_container.visible = true
+	options_menu.visible = false
 
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_button_pressed)
 	options_button.button_down.connect(on_options_button_pressed)
 	exit_button.button_down.connect(on_exit_button_pressed)
+	options_menu.back_to_main_menu.connect(on_exit_options_menu)
 	
---
+++ main_menu/options_menu.gd
@@ -0,0 +1,19 @@
+class_name OptionsMenu
+extends Control
+
+@onready var back_button: Button = $ContentMarginContainer/BackButton
+
+signal back_to_main_menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	back_button.button_down.connect(on_back_pressed)
+	visible = false
+	set_process(true)
+
+func on_back_pressed() -> void:
+	back_to_main_menu.emit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
