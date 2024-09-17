aa4a5c680f81e83049a5d4081d90391b3fddc19f
Theodor Östling <theos327@student.liu.se>
2024-09-17 18:10:47 +0200

    layerMask

--- maps/map_generation/MapGeneration.gd
+++ maps/map_generation/MapGeneration.gd
@@ -18,7 +18,7 @@ func generate_tiles():
 	
 	for position in GenerateMapVariables.random_Object_cells:
 		var random_number = randi() % 16 + 1
-		place_large_object_from_tile(random_number, position)
+		place_large_object_from_new_tile(random_number, position)
 		
 	for position in GenerateMapVariables.random_WaterObject_cells:
 		var random_number = randi() % 5 + 1
@@ -77,6 +77,82 @@ func place_large_water_object_from_tile(objectID, center):
 		for y in range(rows):
 			var tile_position = Vector2(start.x + x, start.y + y)
 			set_cell(2, Vector2i(center.x + x, center.y + y), 16, tile_position)
+
+func place_large_object_from_new_tile(objectID, center):
+	var columns = 0
+	var rows = 0
+	var start = null
+
+	match objectID:
+		1:
+			start = Vector2(0, 0)
+			columns = 2
+			rows = 2
+		2:  
+			start = Vector2(0, 2)  
+			columns = 2
+			rows = 2
+		3:
+			start = Vector2(2, 2)  
+			columns = 2
+			rows = 2
+		4:
+			start = Vector2(4, 2)  
+			columns = 4 
+			rows = 4
+		5:
+			start = Vector2(0, 4)  
+			columns = 4 
+			rows = 4
+		6:
+			start = Vector2(6, 2)  
+			columns = 1
+			rows = 1
+		7:
+			start = Vector2(6, 3)  
+			columns = 1
+			rows = 1
+		8:
+			start = Vector2(0, 4)  
+			columns = 1
+			rows = 1
+		9:
+			start = Vector2(3, 4)  
+			columns = 1
+			rows = 1
+		10:
+			start = Vector2(4, 4)  
+			columns = 1
+			rows = 1
+		11:
+			start = Vector2(9, 5)  
+			columns = 1
+			rows = 1
+		12:
+			start = Vector2(10, 6)  
+			columns = 1
+			rows = 1
+		13:
+			start = Vector2(10, 7)  
+			columns = 4 
+			rows = 1
+		14:
+			start = Vector2(13, 8)  
+			columns = 1
+			rows = 2																						
+		15:
+			start = Vector2(4, 2)  
+			columns = 2
+			rows = 2
+		16: 
+			start = Vector2(0, 2)  
+			columns = 2
+			rows = 2
+	for x in range(columns):
+		for y in range(rows):
+			var tile_position = Vector2(start.x + x, start.y + y)
+			set_cell(2, Vector2i(center.x + x, center.y + y), 2, tile_position)
+
 	
 func place_large_object_from_tile(objectID, center):
 	var columns = 0
