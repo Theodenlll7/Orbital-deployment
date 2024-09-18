1816f9af1ebf92c32c95da43257facfe2e9de88e
Emil Wallberg <ew2001@live.se>
2024-09-18 20:22:30 +0200

    UI Grab focus keyboard and controller support for menus

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,9 +1,11 @@
 class_name MainMenu
 extends Control
 
-@onready var select_mission_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/SelectMissionButton
-@onready var options_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/OptionsButton 
-@onready var exit_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/ExitButton 
+@onready
+var select_mission_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/SelectMissionButton
+@onready
+var options_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/OptionsButton
+@onready var exit_button: Button = $ContentMarginContainer/HBoxContainer/VBoxContainer/ExitButton
 @onready var ship: Control = $ship
 
 @onready var animation_player_ship: AnimationPlayer = $ship/AnimationPlayer
@@ -14,39 +16,57 @@ extends Control
 
 @onready var animation_player: AnimationPlayer = $transistion_content/AnimationPlayer
 
-var target_menu: Control = null
+@onready var target_menu: Control = main_menu
+
 
 func _ready() -> void:
 	handle_connecting_signals()
+	first_btn_focus_grab()
+
 
 func on_animation_finished(animation_name: String) -> void:
 	if animation_name == "to_new_scene":
 		change_menu()
 
+
 func on_select_mission_button_pressed() -> void:
 	animation_player.play("to_new_scene")
 	target_menu = mission_select_menu
+
 
 func on_exit_mission_select_menu() -> void:
 	animation_player.play("to_new_scene")
 	target_menu = main_menu
 
-	
+
 func on_options_button_pressed() -> void:
 	target_menu = options_menu
 	main_menu.visible = false
 	options_menu.visible = true
-	
+	first_btn_focus_grab()
+
+
 func on_exit_options_menu() -> void:
-	target_menu = main_menu 
+	target_menu = main_menu
 	main_menu.visible = true
 	options_menu.visible = false
+	first_btn_focus_grab()
+
 
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
-	
+
+
+func first_btn_focus_grab() -> void:
+	var first_btn = target_menu.find_child("*Button", true) as Button
+	if first_btn:
+		first_btn.grab_focus()
+
+
 func change_menu() -> void:
 	animation_player.play("in_new_scene")
+
+	first_btn_focus_grab()
 
 	if target_menu == options_menu:
 		main_menu.visible = false
@@ -57,20 +77,21 @@ func change_menu() -> void:
 		main_menu.visible = false
 		options_menu.visible = false
 		mission_select_menu.visible = true
-	else: 
+	else:
 		animation_player_ship.play("init_dropship")
 		main_menu.visible = true
 		options_menu.visible = false
 		mission_select_menu.visible = false
 
+
 func handle_connecting_signals() -> void:
 	select_mission_button.button_down.connect(on_select_mission_button_pressed)
 	mission_select_menu.back_to_main_menu.connect(on_exit_mission_select_menu)
-		
+
 	options_button.button_down.connect(on_options_button_pressed)
 	options_menu.back_to_main_menu.connect(on_exit_options_menu)
-	
+
 	exit_button.button_down.connect(on_exit_button_pressed)
-	
+
 	var signal_handler = Callable(self, "on_animation_finished")
 	animation_player.connect("animation_finished", signal_handler)
