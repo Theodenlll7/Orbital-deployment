edb140105b26d60163dbd49e16a5cc21d34b26e2
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-25 12:19:17 +0200

    Added back lost shaders from folder restructure

--- ui/main_menu/loading_level.gd
+++ ui/main_menu/loading_level.gd
@@ -1,7 +1,21 @@
 extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var loading_text: Label = $Control/Label/MarginContainer/loadingText
+
+var time: float = 0.0;
+var sinTime: float = 0.0
+var speed: float = 2.0
+var minOpacity = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation_player.play("loading_animation")
+
+func flashLoadingText(sinTime: float) -> void:
+	loading_text.modulate.a = sinTime
+
+func _process(delta: float) -> void:
+	time += delta
+	sinTime = minOpacity + (1 - minOpacity) * ((sin(time * speed) + 1) * 0.5)
+	flashLoadingText(sinTime)
