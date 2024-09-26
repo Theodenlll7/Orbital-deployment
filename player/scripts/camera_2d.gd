934888e087db456d89e3307a89e239087c87e557
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-26 22:28:55 +0200

    Added camera movment in game

--- player/scripts/camera_2d.gd
+++ player/scripts/camera_2d.gd
@@ -1,6 +1,14 @@
 extends Camera2D
 
+var follow_strength: float = 100.0  
+var lerp_factor: float = 1000.0     
 
-# Screen shake function using coroutines
 func screen_shake(_intensity: float = 10.0, _duration: float = 0.5, _shakes: int = 5) -> void:
 	pass
+
+func _process(delta: float) -> void:
+	var mouse_pos_2d = get_viewport().get_mouse_position()
+	var mouse_offset = mouse_pos_2d - get_viewport().get_visible_rect().size / 2
+	var target_position = mouse_offset.normalized() * follow_strength
+	
+	position = lerp(Vector2(), target_position, mouse_offset.length() / lerp_factor)
