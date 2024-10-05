c9574bc6b9d1482d0e10e940c0d2499c6db2a4be
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-10-05 20:28:49 +0200

    Changed some y sorting and lights

--- maps/terrains/bake_navigation.gd
+++ maps/terrains/bake_navigation.gd
@@ -5,16 +5,16 @@ extends TileMapLayer
 @export var physics_layers: Array[int] = [0]
 
 
-func _tile_data_runtime_update(_coords: Vector2i, tile_data: TileData) -> void:
-	tile_data.set_navigation_polygon(0, null)
-
-
-func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
-	for obstacles in obstacle_layers:
-		for id in physics_layers:
-			if (
-				coords in obstacles.get_used_cells()
-				and obstacles.get_cell_tile_data(coords).get_collision_polygons_count(id) != 0
-			):
-				return true
-	return false
+#func _tile_data_runtime_update(_coords: Vector2i, tile_data: TileData) -> void:
+	#tile_data.set_navigation_polygon(0, null)
+#
+#
+#func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
+	#for obstacles in obstacle_layers:
+		#for id in physics_layers:
+			#if (
+				#coords in obstacles.get_used_cells()
+				#and obstacles.get_cell_tile_data(coords).get_collision_polygons_count(id) != 0
+			#):
+				#return true
+	#return false
