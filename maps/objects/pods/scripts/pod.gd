86cd18675dd0b7f686eb38ea490df2d2baacda96
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-10-09 15:00:48 +0200

    Color fix for shops

--- maps/objects/pods/scripts/pod.gd
+++ maps/objects/pods/scripts/pod.gd
@@ -1,44 +1,42 @@
 extends Node2D
 
 @onready var interaction_area := $InteractionArea
-@onready var storeUI := $CanvasLayer/StoreUI as PodShop
-@onready var Store : PodShop = $CanvasLayer/StoreUI
+@onready var store := $CanvasLayer/StoreUI as PodShop
 
 @export var pod_type = ""
-
-var costumer: Player
+		
 
 func _ready() -> void:
 	interaction_area.interact = _on_interact
-	storeUI.hide()
+	store.hide()
 	set_pod_type(pod_type)
 
 func set_pod_type(new_pod_type):
 	match new_pod_type:
 		"weapon":
-			Store.setLabelsAndCost(PodShop.ShopType.weapon)
+			store.setLabelsAndCost(PodShop.ShopType.weapon)
 		"explosive":
-			Store.setLabelsAndCost(PodShop.ShopType.explosive)
+			store.setLabelsAndCost(PodShop.ShopType.explosive)
 			
 			
 func _on_interact(player : Player):
 	showUI(player)
 
 func showUI(player : Player):
-	storeUI.show()
-	storeUI.costumer = player.inventory
+	store.show()
+	store.costumer = player.inventory
 
 
 func hideUI():
-	storeUI.hide()
-	storeUI.costumer = null
+	store.hide()
+	store.costumer = null
 
 
 func _input(event: InputEvent) -> void:
 	# Check if the event is a key press and if it's the ESC key
 	if event.is_action_pressed("ui_cancel"):
 		# Hide the UI when ESC is pressed
-		if storeUI.visible:
+		if store.visible:
 			hideUI()
 
 
--- maps/objects/pods/scripts/weapon_pod.gd
+++ maps/objects/pods/scripts/weapon_pod.gd
@@ -1,6 +1,6 @@
 class_name PodShop extends Control
 
-@export var ammo_cost: int = 200
+@export var ammo_cost: int = 225
 
 @export var weapon_buy_button: PackedScene
 
@@ -14,13 +14,14 @@ var explosives: Array[ExplosiveResource] = []
 
 var pod_type: ShopType = ShopType.weapon
 
-var costumer: Inventory
-
+var costumer: Inventory:
+	set(value):
+		costumer = value
+		updatePlayerMoney()
 
 func _ready():
 	setRefillAmmonition()
 	loadResources()
-
 
 func loadResources():
 	var paths = FilePaths.get_files(weapons_dir, ".tres")
@@ -37,6 +38,10 @@ func setRefillAmmonition():
 	buy_btn.action_mode = BaseButton.ACTION_MODE_BUTTON_RELEASE
 	buy_btn.connect("pressed", _on_refill_ammonition_button_pressed.bind(ammo_cost))
 
+func updatePlayerMoney() -> void:
+		if costumer:
+			var player_money = $HeaderPanelContainer/MarginContainer/HBoxContainer.get_node("Players_money")	
+			player_money.text = str(costumer.money) + "$"
 
 func setLabelsAndCost(shop_type: ShopType):
 	var array_items: Array
@@ -48,9 +53,15 @@ func setLabelsAndCost(shop_type: ShopType):
 			array_items = explosives
 
 	var shop = $ContentPanelContainer/MarginContainer/VBoxContainer/ScrollContainer/shop
+
 	if shop_type != ShopType.weapon:
 		var refil_ammo_shop_slot: Panel = $ContentPanelContainer/MarginContainer/VBoxContainer/Panel
 		refil_ammo_shop_slot.visible = false
+	else:
+		var ammo_cost_label: Label = $ContentPanelContainer/MarginContainer/VBoxContainer/Panel/RefillAmmonition/MarginContainer/HBoxContainer/Cost
+		ammo_cost_label.text = str(ammo_cost) + " $"
+		
+	
 
 	for item in array_items:
 		var buy_btn: Button = weapon_buy_button.instantiate()
@@ -66,13 +77,33 @@ func setLabelsAndCost(shop_type: ShopType):
 
 func updateButtonLabels():
 	var player_money = $HeaderPanelContainer/MarginContainer/HBoxContainer.get_node("Players_money")
-	player_money.text = str(costumer.money) + "$"
-
+	updateCostLabelColor(player_money)
+	updatePlayerMoney()
+	
+func updateCostLabelColor(player_money) -> void:
+	var shop = $ContentPanelContainer/MarginContainer/VBoxContainer/ScrollContainer/shop
+	
+	var ammo_cost_label: Label = $ContentPanelContainer/MarginContainer/VBoxContainer/Panel/RefillAmmonition/MarginContainer/HBoxContainer/Cost
+	if costumer.money >= int(ammo_cost_label.text.split(" ")[0]): 
+		ammo_cost_label.set("theme_override_colors/font_color", Color(31.0/255.0, 186.0/255.0, 79.0/255.0)) 
+	else:
+		ammo_cost_label.set("theme_override_colors/font_color", Color(0.8, 0.1, 0.1)) 
+	
+	for buy_btn in shop.get_children():
+		var pod_item_container = buy_btn.get_child(0).get_child(0) 
+		var cost: Label = pod_item_container.get_node_or_null("Cost")
+		var item_cost = int(cost.text.split(" ")[0])
+		if cost:
+			if costumer.money >= int(cost.text.split(" ")[0]): 
+				cost.set("theme_override_colors/font_color", Color(31.0/255.0, 186.0/255.0, 79.0/255.0)) 
+			else:
+				cost.set("theme_override_colors/font_color", Color(0.8, 0.1, 0.1)) 
 
 func _on_refill_ammonition_button_pressed(cost: int) -> void:
 	if costumer.money >= cost:
 		costumer.money -= cost
 		costumer.add_ammo()
+	updateButtonLabels()
 
 
 func _on_button_pressed(identifier: StringName) -> void:
