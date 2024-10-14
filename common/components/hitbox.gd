31fef4fc3f55d731a2065d3a9418e80355809d8f
Emil Wallberg <ew2001@live.se>
2024-10-14 19:35:39 +0200

    Fixed problem with sniper bullets traveling to fast

--- common/components/hitbox.gd
+++ common/components/hitbox.gd
@@ -1,13 +1,35 @@
 class_name Hitbox extends Area2D
 
 @export var damage: int = 10
+@export var cast_shape: Shape2D  # This will be the shape for the hitbox
+@export var cast_margin: float = 0.1  # A small margin to avoid tunneling issues
+
+@onready var last_position: Vector2 = global_position
 
 
-func _ready() -> void:
-	area_entered.connect(_on_area_enterd)
+func _physics_process(_delta: float) -> void:
+	# Perform shape casting from the last position to the current position
+	ray_cast_to_current_position()
+	# Update last position to the current position
+	last_position = global_position
 
 
-func _on_area_enterd(area: Area2D):
-	if area is Hurtbox and !area.is_invulnerable:
-		area.damage(damage)
-		get_parent().queue_free()
+# Function to perform the shape cast from last_position to the current global position
+func ray_cast_to_current_position() -> void:
+	# Get the space state (physics world)
+	var space_state = get_world_2d().direct_space_state
+
+	var query = PhysicsRayQueryParameters2D.create(
+		global_position, last_position, collision_mask, [self]
+	)
+	query.collide_with_areas = true
+	query.collide_with_bodies = false
+
+	# Cast the shape from last_position to the current global position
+	var result = space_state.intersect_ray(query)
+
+	if result:
+		var collider = result["collider"]
+		if collider is Hurtbox and not collider.is_invulnerable:
+			collider.damage(damage)
+			get_parent().queue_free()
